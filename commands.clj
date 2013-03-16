5505e3d668e97a665f63518169d625244cbe6853
Siva Jagadeesan <sivajag@gmail.com>
2013-03-16 16:45:49 -0700

    V1 commands fixed

--- commands.clj
+++ commands.clj
@@ -1,34 +1,43 @@
 (require '[clj-http.client :as client])
 
+(defn my-pp [m]
+  (clojure.pprint/pprint (select-keys m [:status :body])))
+
 ;; Version 1
 ;; ---------
 
 ;; List Orders
-(pprint (client/get "http://localhost:4000/api" {:query-params {"action" "list_orders"}
-                                                 :as :json}))
+
+(clj-http.client/get "http://localhost:4000/api" {:query-params {"action" "list_orders"}
+                                                  :as :json})
 
 ;; Find Order 1
-(pprint (client/get "http://localhost:4000/api" {:query-params {"action" "view_order"
-                                                                "order_id" "1"}
-                                                 :as :json}))
+
+(clj-http.client/get "http://localhost:4000/api" {:query-params {"action" "view_order"
+                                                                 "order_id" "1"}
+                                                  :as :json})
 ;; Delete PAID order
-(pprint (client/get "http://localhost:4000/api" {:query-params {"action" "cancel_order"
-                                                                "order_id" "1"}
-                                                 :as :json}))
+
+(clj-http.client/get "http://localhost:4000/api" {:query-params {"action" "cancel_order"
+                                                                 "order_id" "1"}
+                                                  :as :json})
 
 ;; Find Order 1
-(pprint (client/get "http://localhost:4000/api" {:query-params {"action" "view_order"
-                                                                "order_id" "1"}
-                                                 :as :json}))
+
+(clj-http.client/get "http://localhost:4000/api" {:query-params {"action" "view_order"
+                                                                 "order_id" "1"}
+                                                  :as :json})
 
 ;; Delete SHIPPED order
-(pprint (client/get "http://localhost:4000/api" {:query-params {"action" "cancel_order"
-                                                                "order_id" "2"}
-                                                 :as :json}))
+
+(clj-http.client/get "http://localhost:4000/api" {:query-params {"action" "cancel_order"
+                                                                 "order_id" "2"}
+                                                  :as :json})
 
 ;; Delete already cancelled order
-(pprint (client/get "http://localhost:4000/api" {:query-params {"action" "cancel_order"
-                                                                "order_id" "1"}
-                                                 :as :json}))
+
+(clj-http.client/get "http://localhost:4000/api" {:query-params {"action" "cancel_order"
+                                                                 "order_id" "1"}
+                                                  :as :json})
 
 
